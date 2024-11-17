a7cdc759ab86e0b802fb08e1da883e6199b7c382
ilayk <ilay.konovalov@mail.ru>
2024-11-18 04:51:55 +0500

    quest 2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\FormProcessor;
+use App\Models\Groups;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,16 @@ Route::get('/', function () {
 
 Route::get('/useform', [FormProcessor::class, 'index']);
 Route::post('/store_form', [FormProcessor::class, 'store'])->name("form.store");
+Route::get('/test/database', function () {
+    $group = new Groups;
+
+    $group->name = 'Test Group';
+    $group->description = 'This is a test group.';
+
+    if ($group->save()) {
+        return response('Group created successfully!', 201);
+    } else {
+        return response('Failed to create group.', 500);
+    }
+});
+
