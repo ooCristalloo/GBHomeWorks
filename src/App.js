351ac1fcb5cbc93823125cf0f7ec256c58756db0
ilayk <ilay.konovalov@gmail.com>
2025-03-07 03:05:43 +0500

    theme

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import HomePage from "./components/HomePage";
-import AboutPage from "./components/AboutPage";
 import {BrowserRouter, Route, Router, Routes} from "react-router-dom";
+import store from  './components/store/store';
+import {Provider} from 'react-redux'
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<HomePage/>}/>
-                <Route path="/about" element={<AboutPage/>}/>
-            </Routes>
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<HomePage/>}/>
+                </Routes>
+            </BrowserRouter>
+        </Provider>
     );
 }
 
